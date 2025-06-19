cbbcc95cefae510e18de31c6007823f3ed61836a
raulbellidosalas <u202310931@upc.edu.pe>
2025-06-19 15:23:24 -0500

    fixed errors with the creation of users and projects

--- arquitech-back-end/src/main/java/com/acme/arquitech/platform/ArquitechBackEndApplication.java
+++ arquitech-back-end/src/main/java/com/acme/arquitech/platform/ArquitechBackEndApplication.java
@@ -2,8 +2,10 @@ package com.acme.arquitech.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Enables JPA auditing for @CreatedDate and @LastModifiedDate
 public class ArquitechBackEndApplication {
 
     public static void main(String[] args) {
